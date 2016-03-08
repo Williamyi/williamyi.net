3775cfc734546ae7e2ff347b09895e9463d0ff46
William YI <Yiwill21@gmail.com>
2016-03-08 12:53:46 -0800

    time
    add the date and time

--- js/final.js
+++ js/final.js
@@ -1,3 +1,43 @@
+var vReturnback = document.getElementById("returnback");
+
+function validateFirstname(){
+  var vFirstname = document.getElementById("firstname").value;
+  if (vFirstname===null || vFirstname===""){
+    vReturnback.textContent ="Firstname is REQUIRED";
+    return false;
+  } else
+    return true;
+}
+function validateLastname(){
+  var vLastname = document.getElementById("lastname").value;
+  if (vLastname===null || vLastname===""){
+    vReturnback.textContent ="Lastname is REQUIRED";
+    return false;
+  } else
+    return true;
+}
+function validateEmail() {
+  var vEmail = document.getElementById("email").value;
+  var atpos = vEmail.indexOf("@");
+  var dotpos = vEmail.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= vEmail.length || vEmail === null || vEmail === "") {
+    vFeedback.textContent = "Not a valid e-mail address";
+    return false;
+  } else
+    return true;
+}
+function validateForm() {
+  return validateFirstname() && validateLastname();
+}
+
+
+
+
+
+
+
+
+
 function SetDate() {
   var myDate = new Date();
   var d = myDate.toDateString();
